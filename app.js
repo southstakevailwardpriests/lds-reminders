5fb09974a28c3f7c9becdc057e976826414c5817
southstakevailwardpriests <southstakevailwardpriests@gmail.com>
2016-10-09 14:45:58 -0700

    Added session store

--- app.js
+++ app.js
@@ -11,11 +11,21 @@ var url = oauth2Client.generateAuthUrl({
 });
 var app = require('express')();
 var session = require('express-session');
+app.use(session({
+    store:new (require('connect-pg-simple')(session))(),
+    secret:process.env.SESS_SECRET,
+    resave:false,
+    cookie:{maxAge:7*24*60*60*1000}
+}));
 var server = require('http').createServer(app);
 app.get('/',function(req,res){
     res.redirect(url);
 });
 app.get('/oathCallback',function(req,res){
-    res.send(req.query.code);
+    req.session.code = req.query.code;
+    res.redirect("/app");
+});
+app.get("/app",function(req,res){
+    res.send(req.session.code);
 });
 server.listen(3000);
